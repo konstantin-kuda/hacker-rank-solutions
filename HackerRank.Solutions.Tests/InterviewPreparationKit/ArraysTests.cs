49c0b923d91b3868035d267dd858eb75d76b90da
kuda <konstantin.kuda@un.org>
2022-02-21 13:38:55 +0100

    HourglassSum converted

---
+++ HackerRank.Solutions.Tests/InterviewPreparationKit/ArraysTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using HackerRank.Solutions.InterviewPreparationKit;
+using NUnit.Framework;
+
+namespace HackerRank.Solutions.Tests.InterviewPreparationKit;
+
+[TestFixture]
+public class ArraysTests
+{
+    [Test]
+    public void HourglassSum_Should_ReturnProperValue_1()
+    {
+        var input = new List<List<int>>
+        {
+            new() { 1, 1, 1, 0, 0, 0 },
+            new() { 0, 1, 0, 0, 0, 0 },
+            new() { 0, 1, 0, 0, 0, 0 },
+            new() { 0, 0, 2, 4, 4, 0 },
+            new() { 0, 0, 0, 2, 0, 0 },
+            new() { 0, 0, 1, 2, 4, 0 }
+        };
+        var result = Arrays.HourglassSum(input);
+        result.Should().Be(19);
+    }
+    
+    [Test]
+    public void HourglassSum_Should_ReturnProperValue_2()
+    {
+        var input = new List<List<int>>
+        {
+            new() {-9, -9, -9,  1,  1,  1},
+            new() { 0, -9,  0,  4,  3,  2},
+            new() {-9, -9, -9,  1,  2,  3},
+            new() { 0,  0,  8,  6,  6,  0},
+            new() { 0,  0,  0, -2,  0,  0},
+            new() { 0,  0,  1,  2,  4,  0}
+        };
+        var result = Arrays.HourglassSum(input);
+        result.Should().Be(28);
+    }
+    
+    [Test]
+    public void HourglassSum_Should_ReturnProperValue_3()
+    {
+        var input = new List<List<int>>
+        {
+            new() { 0, -4, -6, 0, -7, -6 },
+            new() { -1, -2, -6, -8, -3, -1 },
+            new() { -8, -4, -2, -8, -8, -6 },
+            new() { -3, -1, -2, -5, -7, -4 },
+            new() { -3, -5, -3, -6, -6, -6 },
+            new() { -3, -6, 0, -8, -6, -7 }
+        };
+        var result = Arrays.HourglassSum(input);
+        result.Should().Be(-19);
+    }
+}
---
+++ HackerRank.Solutions/InterviewPreparationKit/Arrays.cs
@@ -0,0 +1,40 @@
+﻿namespace HackerRank.Solutions.InterviewPreparationKit;
+
+public class Arrays
+{
+    //https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+    public static int HourglassSum(List<List<int>> arr)
+    {
+        var columnCount = arr[0].Count;
+        for (var i = 1; i < arr.Count; i++)
+        {
+            if (arr[i].Count != columnCount)
+            {
+                throw new Exception($"Invalid array: Row should have {columnCount} columns but has {arr[i].Count}");
+            }
+        }
+
+        var max = 0;
+        for (var rowIndex = 1; rowIndex < arr.Count - 1; rowIndex++)
+        {
+            for (var columnIndex = 1; columnIndex < columnCount - 1; columnIndex++)
+            {
+                var previousRow = arr[rowIndex - 1];
+                var nextRow = arr[rowIndex + 1];
+                var count = previousRow[columnIndex - 1] + previousRow[columnIndex] + previousRow[columnIndex + 1] +
+                            arr[rowIndex][columnIndex] +
+                            nextRow[columnIndex - 1] + nextRow[columnIndex] + nextRow[columnIndex + 1];
+
+                if (count > max)
+                {
+                    max = count;
+                }
+                else if (rowIndex == 1 && columnIndex == 1)
+                {
+                    max = count;
+                }
+            }
+        }
+        return max;
+    }
+}
